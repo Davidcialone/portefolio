017c31b22063b279e928db0f9805e45c908bbaab
Davidcialone <cialonedavid@gmail.com>
2024-11-14 14:59:31 +0100

    adjust routes of mobile navbar

--- src/components/forms/brand.jsx
+++ src/components/forms/brand.jsx
@@ -4,8 +4,8 @@ import Navbar from 'react-bootstrap/Navbar';
 export function MyBrand() {
   return (
     <Navbar>
-      <Container style={{ display: 'flex', alignItems: 'center', padding: '0' }}>
-      <Navbar.Brand href="/" style={{ backgroundColor: '#A1C3DE', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Container style={{ display: 'flex', alignItems: 'center' }}>
+      <Navbar.Brand href="/" style={{ backgroundColor: 'RGB(221, 240, 253)', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
           <img
             alt=""
             src="/portefolio/images/profil2.png"
@@ -16,6 +16,7 @@ export function MyBrand() {
           <div style={{ textAlign: 'center',fontSize: '1rem', fontWeight: 'bold', margin: "1rem" }}>25 ans d'expérience dans la relation client</div>
           <div style={{ textAlign: 'center',fontSize: '1rem', fontWeight: 'bold',  margin: "1rem" }}>15 ans de management</div>
           <div style={{ textAlign: 'center',fontSize: '1rem', fontWeight: 'bold',  margin: "1rem" }}>Développeur en début de carrière</div>
+          <div style={{ textAlign: 'center',fontSize: '1rem', fontWeight: 'bold',  margin: "1rem" }}>Passioné par le sport et ses valeurs</div>
 
 
         </Navbar.Brand>
--- src/components/forms/homePage.jsx
+++ src/components/forms/homePage.jsx
@@ -51,8 +51,8 @@ function HomePage() {
 
       {/* Sidebar (Brand or Nav) */}
       <GridItem boxShadow="0 4px 20px rgba(0, 0, 0, 0.5)" bg={bgColor} area={'nav'}>
-        <Box bg="white" p={4} shadow="lg" borderRadius="md" margin={2}>
-            <MyBrand />
+        <Box bg="white" p={2} shadow="lg" borderRadius="md" >
+          <MyBrand />
         </Box>
       </GridItem>
 
@@ -61,13 +61,13 @@ function HomePage() {
         {/* Stack allows vertical stacking of elements with spacing */}
         <Stack spacing={8}>
           {/* Block 2: Description */}
-          <Box bg="white" p={4} shadow="lg" borderRadius="md" margin={2}>
+          <Box bg="white" p={2} shadow="lg" borderRadius="md" margin={2}>
             <Heading size="lg">A propos de moi</Heading>
             <Description />  {/* Your description component */}
           </Box>
 
           {/* Block 3: Skills */}
-          <Box bg="white" p={4} shadow="lg" borderRadius="md" margin={2}>
+          <Box bg="white" p={2} shadow="lg" borderRadius="md" margin={2}>
             <Skills />  {/* Your skills component */}
           </Box>
         </Stack>
@@ -75,7 +75,7 @@ function HomePage() {
 
       {/* Footer */}
       <GridItem boxShadow="0 4px 20px rgba(0, 0, 0, 0.5)" bg={bgColor} area={'footer'}>
-        <Box bg="white" p={4} shadow="lg" borderRadius="md" margin={2}>
+        <Box bg="white" p={2} shadow="lg" borderRadius="md" margin={2}>
           <Heading size="md">Contact</Heading>
           <ContactFooter />
         </Box>
